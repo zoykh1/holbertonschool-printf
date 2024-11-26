372e1b34fc51b3750e32435715e15f87bf4e4e1c
zoykh1 <kerimgmc45@gmail.com>
2024-11-26 16:43:35 +0100

    main.h

--- main.h
+++ main.h
@@ -1,10 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct format_specifier - structure pour gerer specificateur de formats
+ * @specifier: caractere du specificateur
+ * @func: pointeur vers la fonction qui gere le specificateur
+ */
+
+typedef struct format_specifier
+{
+	char specifier;
+	int (*func)(va_list);
+} format_specifier_t;
+
 int _printf(const char *format, ...);
-int print_char(char c);
-int print_string(char *s);
-int print_percent(void);
-int print_int(int n);
+int _putchar(char c);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+int print_number(int num);
+int print_int(va_list args);
+int handle_specifier(const char specifier, va_list args, format_specifier_t
+specifiers[]);
 
 #endif /* MAIN_H */
